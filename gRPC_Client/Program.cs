d3dff0b557a64d4bee23d1df5875271f4ee734c7
Niyaz <nsh.ayupov@gmail.com>
2023-05-16 14:01:41 +0300

    Пример работы с gRPC (сервер+клиент)

---
+++ gRPC_Client/Program.cs
@@ -0,0 +1,14 @@
+﻿using Grpc.Net.Client;
+using gRPC_Client;
+
+// создаем канал для обмена сообщениями с сервером
+// параметр - адрес сервера gRPC
+using var channel = GrpcChannel.ForAddress("https://localhost:7034");
+// создаем клиент
+var client = new Greeter.GreeterClient(channel);
+Console.Write("Введите имя: ");
+var name = Console.ReadLine();
+// обмениваемся сообщениями с сервером
+var reply = await client.SayHelloAsync(new HelloRequest { Name = name });
+Console.WriteLine($"Ответ сервера: {reply.Message}");
+Console.ReadKey();
